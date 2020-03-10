3ca7ec42253cd4b1f7aebef781a7f229c9c7f875
Nick Ball <naball99@icloud.com>
2020-03-10 12:09:38 -0400

    fix code syntax and format

--- project/app.js
+++ project/app.js
@@ -1,110 +1,109 @@
-var express = require("express"),
-app = express(),
-bodyParser = require("body-parser"),
+var bodyParser = require("body-parser"),
 methodOverride = require("method-override"),
 expressSanitizer = require("express-sanitizer"),
-mongoose = require("mongoose");
+mongoose = require("mongoose"),
+express = require("express"),
+app = express();
 
-
-//config db (mongoDB)
-mongoose.connect("mongodb://localhost:27017/twitterClone", {useNewUrlParser: true}); //change L8r
+// APP CONFIG
+mongoose.connect("mongodb://localhost:27017/TCACSCCGP", {useNewUrlParser: true});
 app.set("view engine", "ejs");
-//to serve custom style sheet:
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
-//db structure (schema)
+// MONGOOSE/MODEL CONFIG
 var postSchema = new mongoose.Schema({
 	title: String,
 	image: String,
 	body: String,
 	created: {type: Date, default: Date.now}
 });
-//declare Post variable
 var Post = mongoose.model("Post", postSchema);
 
-//create ROOT route:
+// RESTFUL ROUTES
+
 app.get("/", function(req, res){
 	res.redirect("/posts");
 });
 
+// INDEX route
 app.get("/posts", function(req, res){
-	Post.find({}, function(err. posts){
+	Post.find({}, function(err, posts){
 		if(err){
-		console.log("error");
-	} else {
-		res.render("index", {posts: posts});
+			console.log("error!");
+		} else {
+			res.render("index", {posts: posts});
 		}
 	});
 });
-
-//after where it says res.render(“index", {posts: posts}): add this (line ~39):
-app.get("/posts/new", function(req, res){
+// NEW route
+app.get("/posts/new", function(req ,res){
 	res.render("new");
 });
 
-app.post(“/posts", function(req, res){
-	//create blog
+//CREATE route
+app.post("/posts", function(req, res){
+	// create post
 	req.body.post.body = req.sanitize(req.body.post.body)
-	Post.create(req.body.blog, function(err, newPost){
-	if(err){
-		res.render(“new");
-	} else {
-	//then redirect to index page
-	res.redirect(“/posts");
-	}
-      });
-});
-// furthur under that route in app.js, time for the show route:
-app.get(“/posts/:id", function(req, res){
-	Post.findById(req.params.id, function(err, foundPost){
+	Post.create(req.body.post, function(err, newPost){
 		if(err){
-			res.redirect(“/posts");
+			res.render("new");
 		} else {
-			res.render(“show", {post: foundPost});
-		}
-	})
-});
-
-app.get(“/posts/:id/edit", function(req, res){
-	Post.findById(req.params.id, function(err, foundPost){
-		if(err){
-			res.redirect(“/posts");
-		} else {
-			res.render(“edit", {post: foundPost});
+		// then redirect to index
+			res.redirect("/posts");
 		}
 	});
 });
 
-app.put(“/posts/:id", function(req, res){
-	req.body.post.body = req.sanitize(req.body.post.body)
-	Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, updatedPost){
-	if(err){
-		res.redirect(“/posts");
-	} else {
-		res.redirect(“/posts/" + req.params.id);
-	}
-     });
+//SHOW route
+app.get("/posts/:id", function(req, res){
+	Post.findById(req.params.id, function(err, foundPost){
+		if(err){
+			res.redirect("/posts");
+		} else {
+			res.render("show", {post: foundPost});
+		}
+	});
 });
 
-//delete route
-app.delete(“/posts/:id", function(req, res){
+//EDIT route
+app.get("/posts/:id/edit", function(req, res){
+	Post.findById(req.params.id, function(err, foundPost){
+		if(err){
+			res.redirect("/posts");
+		} else {
+			res.render("edit", {post: foundPost});
+		}
+	});
+});
+
+//UPDATE route
+app.put("/posts/:id", function(req, res){
+	req.body.post.body = req.sanitize(req.body.post.body)
+	Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, updatedPost){
+		if(err){
+			res.redirect("/posts");
+		} else {
+			res.redirect("/posts/" + req.params.id);
+		}
+	});
+});
+
+//DELETE route
+app.delete("/posts/:id", function(req, res){
 	//destroy post
 	Post.findByIdAndRemove(req.params.id, function(err){
 		if(err){
-			res.redirect(“/posts");
+			res.redirect("/posts");
 		} else {
-			res.redirect(“/posts");
+			res.redirect("/posts");
 		}
-	})
-	//redirect
+	});
+	//redirect somewhere
 });
 
-
-
-//to run
 app.listen(process.env.PORT, process.env.IP, function(){
-	console.log("server is up");
-}) //to run: PORT=3000 node app.js
+	console.log("Server is Running!");
+})
